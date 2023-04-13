a0ec9f2540e52a4505f21e13f5ea611cd8453b45
David Smith <dasmith@zero-to-prod.com>
2023-04-13 09:46:42 -0400

    Create a query in the `WelcomeController.php` to pass data to the view.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
@@ -10,6 +11,26 @@ class WelcomeController extends Controller
 {
     public function __invoke(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
-        return view('welcome');
+
+        $comments = collect(DB::select(
+            <<<SQL
+                SELECT
+                    orderid,
+                    comments,
+                    CASE
+                        WHEN comments like '%candy%' THEN 'candy'
+                        WHEN comments like '%honey%' THEN 'candy'
+                        WHEN comments like '%smarties%' THEN 'candy'
+                        WHEN comments like '%taffy%' THEN 'candy'
+                        WHEN comments like '%call%' THEN 'call'
+                        WHEN comments like '%refer%' THEN 'refer'
+                        WHEN comments like '%signature%' THEN 'signature'
+                    END as type
+                FROM
+                    sweetwater_test;
+                SQL
+        ));
+
+        return view('welcome')->with('comments', $comments);
     }
 }
