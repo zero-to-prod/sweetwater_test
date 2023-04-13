9af591785e1e729af0e288cbad79015300e6e7ba
David Smith <dasmith@zero-to-prod.com>
2023-04-13 09:42:34 -0400

    Updated `welcome.blade.php` to display the comments and group them into the following sections based on what the comment was about: - Comments about candy - Comments about call me / don't call me - Comments about who referred me - Comments about signature requirements upon delivery - Miscellaneous comments (everything else)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,68 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Web Programmer Test Project</title>
-    </head>
-    <body>
+    <title>Web Programmer Test Project</title>
+</head>
+<body>
+<h1>Web Programmer Test Project</h1>
+<h2>Candy</h2>
+<table>
+    <tr>
+        <th>Comment</th>
+    </tr>
+    @foreach($comments->filter(fn($comment) => $comment->type === 'candy') as $comment)
+        <tr>
+            <td>{{$comment->comments}}</td>
+        </tr>
+    @endforeach
+</table>
 
-    </body>
+<h2>Call</h2>
+<table>
+    <tr>
+        <th>Comment</th>
+    </tr>
+    @foreach($comments->filter(fn($comment) => $comment->type === 'call') as $comment)
+        <tr>
+            <td>{{$comment->comments}}</td>
+        </tr>
+    @endforeach
+</table>
+<h2>Refer</h2>
+<table>
+    <tr>
+        <th>Comment</th>
+    </tr>
+    @foreach($comments->filter(fn($comment) => $comment->type === 'refer') as $comment)
+        <tr>
+            <td>{{$comment->comments}}</td>
+        </tr>
+    @endforeach
+</table>
+<h2>Signature</h2>
+<table>
+    <tr>
+        <th>Comment</th>
+    </tr>
+    @foreach($comments->filter(fn($comment) => $comment->type === 'signature') as $comment)
+        <tr>
+            <td>{{$comment->comments}}</td>
+        </tr>
+    @endforeach
+</table>
+<h2>Other</h2>
+<table>
+    <tr>
+        <th>Comment</th>
+    </tr>
+    @foreach($comments->filter(fn($comment) => $comment->type === null) as $comment)
+        <tr>
+            <td>{{$comment->comments}}</td>
+        </tr>
+    @endforeach
+</table>
+</body>
 </html>
